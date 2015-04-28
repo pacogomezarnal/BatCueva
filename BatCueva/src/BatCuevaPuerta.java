01cba0d66025c6060d787d2c5976667aa72c8d35
pacogomezarnal <edutic.es@gmail.com>
2015-04-28 12:08:24 +0200

    Hemos añadido un JTextField de usuario
    Signed-off-by: pacogomezarnal

--- BatCueva/src/BatCuevaPuerta.java
+++ BatCueva/src/BatCuevaPuerta.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextPane;
 
 //La BatPuerta tiene ahora un Absolute Layout
 
@@ -21,6 +22,9 @@ public class BatCuevaPuerta extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JTextPane usuario = new JTextPane();
+		usuario.setBounds(34, 10, 141, 25);
+		contentPane.add(usuario);
 	}
-
 }
