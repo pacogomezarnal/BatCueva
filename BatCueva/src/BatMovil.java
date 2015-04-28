35dc54dfe53b5b73959198934c42e8ecea038809
pacogomezarnal <edutic.es@gmail.com>
2015-04-28 12:41:46 +0200

    Caja
    Signed-off-by: pacogomezarnal

--- BatCueva/src/BatMovil.java
+++ BatCueva/src/BatMovil.java
@@ -1,10 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 //Documento la parte de BatNovil
 
 public class BatMovil extends JFrame {
+	private JTextField textField;
 	//Esta es la primera aportacion de robin
 
 	/**
@@ -16,7 +18,13 @@ public class BatMovil extends JFrame {
 		JLabel nombre = new JLabel("BatMovil");
 		nombre.setBounds(10, 10, 85, 50);
 		getContentPane().add(nombre);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 54, 86, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
 		setBounds(100, 100, 450, 300);
 
+		
 	}
 }
