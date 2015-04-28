d4569c8e9b796ffbea328a15a073312af8357847
pacogomezarnal <edutic.es@gmail.com>
2015-04-28 11:51:29 +0200

    BatMovil ahora es un JFrame
    Signed-off-by: pacogomezarnal <edutic.es@gmail.com>

--- BatCueva/src/BatMovil.java
+++ BatCueva/src/BatMovil.java
@@ -1,8 +1,9 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 //Documento la parte de BatNovil
 
-public class BatMovil extends JPanel {
+public class BatMovil extends JFrame {
 	//Esta es la primera aportacion de robin
 
 	/**
